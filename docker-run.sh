61dcfbacf240738006b18f910185dddd05fc8c05
Yinyin Qian <yyqian@live.com>
2016-10-11 13:17:06 +0800

    auto-sync

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,16 @@
-docker build -t yyqian/aliyun-ddns .
-docker run -d -p 1987:8080 yyqian/aliyun-ddns
+#!/usr/bin/env bash
+SOURCE_PORT=8080
+TARGET_PORT=1987
+IMAGE_NAME="yyqian/aliyun-ddns"
+CONTAINER_NAME="aliyun-ddns"
+# stop old build
+docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}
+# docker build
+docker build -t ${IMAGE_NAME} .
+# docker run
+docker run -d \
+-p ${TARGET_PORT}:${SOURCE_PORT} \
+--name ${CONTAINER_NAME} \
+${IMAGE_NAME}
+# clean up dangling images
+docker rmi $(docker images -f "dangling=true" -q)
